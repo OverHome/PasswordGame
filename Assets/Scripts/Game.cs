0464aa12d4415a10842faa6050676c0f7e630e9e
KopIAkash1 <andreymart01@gmail.com>
2023-08-06 15:28:50 +0300

    fix 2

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -21,7 +21,7 @@ public class Game : MonoBehaviour
 
     private string[] rim = new[] { "I", "V", "X", "L", "C", "D", "M" };
     private string capcha;
-
+    public TextMeshProUGUI text;
     private void Start()
     {
         capcha = RandomCapcha();
@@ -30,44 +30,89 @@ public class Game : MonoBehaviour
 
     public void CheckPassword()
     {
+        bool checker = false;
         string passwod = textEditor.text;
+        string check_result = null;
+        
         Debug.ClearDeveloperConsole();
         if (passwod.Length < 5)
         {
             print("Должен содержать 5 символов");
-        }
-        else if (!passwod.Any(p=> "123456789".Contains(p)))
+            checker = true;
+            check_result += "<color=red>Должен содержать 5 символов</color>\n";
+        } else { check_result += "<color=green>Должен содержать 5 символов</color>\n"; }
+        if (!passwod.Any(p => "123456789".Contains(p)))
         {
             print("Должен содержать хотя бы 1 цифру");
-        }
-        else if (!passwod.Any(p=> char.IsUpper(p)))
+            checker = true;
+            check_result += "<color=red>Должен содержать хотя бы 1 цифру</color>\n";
+        } else { check_result += "<color=green>Должен содержать хотя бы 1 цифру</color>\n"; }
+        if (!passwod.Any(p => char.IsUpper(p)))
         {
             print("Должен содержать большую букву");
-        }
-        else if (!passwod.Any(p=> char.IsLetterOrDigit(p)))
+            checker = true;
+            check_result += "<color=red>Должен содержать большую букву</color>\n";
+        } else { check_result += "<color=green>Должен содержать большую букву</color>\n"; }
+        if (!passwod.Any(p => !char.IsLetterOrDigit(p))) // Починил не рабочий метод на проверку спец. символа
         {
             print("Должен содержать специальный символ");
-        }else if (passwod.Sum(p=> "123456789".Contains(p)? Convert.ToInt16((Convert.ToString(p))): 0)!=35)
+            checker = true;
+            check_result += "<color=red>Должен содержать специальный символ</color>\n";
+        } else { check_result += "<color=green>Должен содержать специальный символ</color>\n"; }
+        if (passwod.Sum(p => "123456789".Contains(p) ? Convert.ToInt16((Convert.ToString(p))) : 0) != 35)
         {
             print("Сумма всех цифр должна быть равно 35");
-        }else if (!manth.Any(m=> passwod.Contains(m)))
+            checker = true;
+            check_result += "<color=red>Сумма всех цифр должна быть равно 35</color>\n";
+        } else { check_result += "<color=green>Сумма всех цифр должна быть равно 35</color>\n"; }
+        if (triple_check(passwod))
+        {
+            print("Не должен содержать три подряд одинаковых числа");
+            checker = true;
+            check_result += "<color=red>Не должен содержать три подряд одинаковых числа</color>\n";
+        } else { check_result += "<color=green>Не должен содержать три подряд одинаковых числа</color>\n"; }
+        if (!manth.Any(m => passwod.Contains(m)))
         {
             print("Должен содержать месяц года");
-        }
-        else if (!rim.Any(r=> passwod.Contains(r)))
+            checker = true;
+            check_result += "<color=red>Должен содержать месяц года</color>\n";
+        } else { check_result += "<color=green>Должен содержать месяц года</color>\n"; }
+        if (!rim.Any(r => passwod.Contains(r)))
         {
             print("Должен содержать римскую цифру");
-        }else if (!passwod.Contains(capcha))
+            checker = true;
+            check_result += "<color=red>Должен содержать римскую цифру</color>\n";
+        } else { check_result += "<color=green>Должен содержать римскую цифру</color>\n"; }
+        if (!passwod.Contains(capcha))
         {
             print($"Должен содержать нашу капчу: \"{capcha}\"");
-        }
-        else if(!passwod.Contains(DateTime.Now.Day.ToString()))
+            checker = true;
+            check_result += $"<color=red>Должен содержать нашу капчу: \"{capcha}\"</color>\n";
+        } else { check_result += $"<color=green>Должен содержать нашу капчу: \"{capcha}\"</color>\n"; }
+        if (!passwod.Contains(DateTime.Now.Day.ToString()))
         {
             print("Должен содержать сегоднишни день");
-        }else 
+            checker = true;
+            check_result += "<color=red>Должен содержать сегоднишни день</color>\n";
+        } else { check_result += "<color=green>Должен содержать сегоднишни день</color>\n"; }
+        if (checker == false)
         {
             print("Хороший пароль");
         }
+        text.text = check_result;
+    }
+
+    // ВСПОМОГАТЕЛЬНЫЙ МЕТОД ДЛЯ ПРОВЕРКИ ТРИ ИДУЩИХ ПОДРЯД ЧИСЛА
+    private static bool triple_check(string password) 
+    {
+        for (int i = 0; i < (password.Length - 2); i++)
+        {
+            if (password[i] == password[i + 1] && password[i + 1] == password[i + 2])
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private string RandomCapcha()
@@ -80,7 +125,7 @@ public class Game : MonoBehaviour
         {
             stringChars[i] = chars[random.Next(chars.Length)];
         }
-
+        stringChars[random.Next(0, 4)] = chars[^random.Next(1, 8)]; // гарантированное число
         return new String(stringChars);
     }
 }
